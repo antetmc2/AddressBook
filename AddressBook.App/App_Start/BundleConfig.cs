bfeb65717961cf508cae754a188811d81928b199
antetmc2 <antetmc2@gmail.com>
2016-06-25 01:13:30 +0200

    Ubačen Angular & početak dorada za Login i Register

--- AddressBook.App/App_Start/BundleConfig.cs
+++ AddressBook.App/App_Start/BundleConfig.cs
@@ -26,6 +26,10 @@ namespace AddressBook.App
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/bundles/angular").Include(
+                      "~/Scripts/angular.js",
+                      "~/Scripts/angular-messages.js"));
         }
     }
 }
---
+++ AddressBook.App/Scripts/Pages/user.js
@@ -0,0 +1,33 @@
+﻿var myApp = angular.module('UserApp', []);
+
+myApp.controller('UserController', function ($scope) {
+    $scope.test = function () {
+        alert('AAAAAA');
+    };
+});
+
+myApp.factory('UserService', function ($http) {
+    var UserService = {};
+
+    ContactService.Login = function (user) {
+        var response = $http({
+            method: "post",
+            url: "http://localhost:31276/Data/Login",
+            data: JSON.stringify(user),
+            dataType: "json"
+        });
+        return response;
+    };
+
+    ContactService.Register = function (user) {
+        var response = $http({
+            method: "post",
+            url: "http://localhost:31276/Data/Register",
+            data: JSON.stringify(user),
+            dataType: "json"
+        });
+        return response;
+    };
+
+    return UserService;
+});
