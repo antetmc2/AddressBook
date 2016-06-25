e35ab706055bf917ac914e146803467cdf184a9d
antetmc2 <antetmc2@gmail.com>
2016-06-25 12:32:18 +0200

    AJAX login / registracija skroz funkcionalna, potrebno ubaciti obavijesti

---
+++ AddressBook.App/Controllers/DataController.cs
@@ -0,0 +1,76 @@
+﻿using AddressBook.App.Models;
+using AddressBook.DB;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace AddressBook.App.Controllers
+{
+    public class DataController : Controller
+    {
+        // GET: Data
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public void Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                using (AddressBookEntities db = new AddressBookEntities())
+                {
+                    bool userValid = db.User.Any(user => user.Username == model.Username && user.Password == model.Password);
+
+                    if (userValid)
+                    {
+                        FormsAuthentication.SetAuthCookie(model.Username, false);
+                        var FormsAuthCookie = Response.Cookies[FormsAuthentication.FormsCookieName];
+                        var ExistingTicket = FormsAuthentication.Decrypt(FormsAuthCookie.Value).Name;
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "The user name or password provided is incorrect.");
+                    }
+                }
+
+            }
+        }
+
+        [HttpPost]
+        public void Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                using (AddressBookEntities db = new AddressBookEntities())
+                {
+                    var newUser = db.User.Create();
+                    newUser = new User { Username = model.Username, Password = model.Password, Email = model.Email };
+                    db.User.Add(newUser);
+                    db.SaveChanges();
+
+                    FormsAuthentication.SetAuthCookie(model.Username, false);
+                    var FormsAuthCookie = Response.Cookies[FormsAuthentication.FormsCookieName];
+                    var ExistingTicket = FormsAuthentication.Decrypt(FormsAuthCookie.Value).Name;
+                }
+            }
+        }
+
+        [HttpPost]
+        public void LogOff()
+        {
+            FormsAuthentication.SignOut();
+        }
+
+        public string CurrentUser()
+        {
+            var user = User.Identity.GetUserName();
+            return user;
+        }
+    }
+}
--- AddressBook.App/Scripts/Pages/user.js
+++ AddressBook.App/Scripts/Pages/user.js
@@ -1,32 +1,89 @@
 ﻿var myApp = angular.module('UserApp', []);
 
-myApp.controller('UserController', function ($scope) {
+myApp.controller('UserController', function ($scope, $timeout, UserService) {
     $scope.test = function () {
-        alert('AAAAAA');
+        $scope.user = 'AAA';
+    };
+
+    UserService.GetLoggedUser().then(function (d) {
+        $scope.user = d.data;
+    });
+
+    $scope.logIn = function() {
+        var User = {
+            Username: $scope.username,
+            Password: $scope.password
+        }
+        UserService.Login(User).then(function (d) {
+            UserService.GetLoggedUser().then(function (d) {
+                $scope.user = d.data;
+                $scope.username = '';
+                $scope.password = '';
+            });
+        });
+    };
+
+    $scope.logOff = function () {
+        UserService.LogOff().then(function (d) {
+            UserService.GetLoggedUser().then(function (d) {
+                $scope.user = d.data;
+            });
+        });
+    };
+
+    $scope.register = function () {
+        var User = {
+            Username: $scope.username,
+            Password: $scope.password,
+            ConfirmPassword: $scope.passwordConfirm,
+            Email: $scope.email
+        }
+        UserService.Register(User).then(function (d) {
+            UserService.GetLoggedUser().then(function (d) {
+                $scope.user = d.data;
+                $scope.username = '';
+                $scope.password = '';
+                $scope.passwordConfirm = '';
+                $scope.email = '';
+            });
+        });
     };
 });
 
 myApp.factory('UserService', function ($http) {
     var UserService = {};
 
-    ContactService.Login = function (user) {
+    UserService.Login = function (user) {
         var response = $http({
             method: "post",
-            url: "http://localhost:31276/Data/Login",
+            url: "http://localhost:35949/Data/Login",
             data: JSON.stringify(user),
             dataType: "json"
         });
         return response;
     };
 
-    ContactService.Register = function (user) {
+    UserService.Register = function (user) {
         var response = $http({
             method: "post",
-            url: "http://localhost:31276/Data/Register",
+            url: "http://localhost:35949/Data/Register",
             data: JSON.stringify(user),
             dataType: "json"
         });
         return response;
+    };
+
+    UserService.LogOff = function (user) {
+        var response = $http({
+            method: "post",
+            url: "http://localhost:35949/Data/LogOff",
+            dataType: "json"
+        });
+        return response;
+    };
+
+    UserService.GetLoggedUser = function () {
+        return $http.get('http://localhost:35949/Data/CurrentUser')
     };
 
     return UserService;
